b4a4d322d34e823e95a76a56dd2f4df164cd43e4
kloolly <80373910+kloolly@users.noreply.github.com>
2024-06-16 12:29:19 +0700

    Commit Message

---
+++ Lab1/Prog_1_7.java
@@ -0,0 +1,7 @@
+public class Prog_1_7 {
+
+    public static void main(String[] arg) {
+        System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+        System.out.println(9.5 * 4.5 - 2.5 * 3/ 45.5 - 3.5);
+    }
+}
--- Lab2/LeapYear.java
+++ Lab2/LeapYear.java
@@ -1,7 +1,23 @@
 package Lab2;
 
 public class LeapYear {
-    public static void main(String[]args) {
+    //
+    public static boolean isLeapYear(){
+        int year;
+        year = 2090;
+        if (year%4==0) {
+            if (year%100==0) {
+                return year % 400==0;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 
+
+    public static void main(String[]args) {
+        //
+        isLeapYear();
     }
 }
