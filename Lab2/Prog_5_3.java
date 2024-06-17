039fc31810a57b3e8bff3ab9b1050595196ac4af
kloolly <80373910+kloolly@users.noreply.github.com>
2024-06-17 21:19:52 +0700

    DONE

--- Lab2/Prog_5_3.java
+++ Lab2/Prog_5_3.java
@@ -1,1 +1,12 @@
-aa
+public class Prog_5_3 {
+    public static void main(String[]args) {
+        System.out.printf("%-10s %10s\n","Kilograms","Pounds");
+        for (int kilo=1 ;kilo<=199; kilo=kilo+2) {
+            //System.out.println(kilo);
+            double pound = kilo*2.2;
+            //System.out.println((float) pound);
+
+            System.out.printf("%-10d %,10.1f\n",kilo,pound);
+        }
+    }
+}
