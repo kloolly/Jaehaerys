26c1c6b7470b7d665f7f1a51d7707700e2a87b06
kloolly <80373910+kloolly@users.noreply.github.com>
2024-06-18 21:37:43 +0700

    NOT DONE

---
+++ Lab2/Prog_5_11.java
@@ -0,0 +1,17 @@
+public class Prog_5_11 {
+    public static void main(String[]args) {
+        int counter =0;
+        for (int i=100 ; i <=200 ; i++) {
+            //System.out.printf("%d ",i);
+
+            if (i % 5 ==0 && i % 6 ==0) {
+                System.out.println(i);
+            }
+//
+//            counter++;
+//            if (counter %10 == 0) {
+//            System.out.println(" ");
+//            }
+        }
+    }
+}
