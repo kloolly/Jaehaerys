260805829429d1a817faa976af358d9e80316d49
kloolly <80373910+kloolly@users.noreply.github.com>
2024-06-18 23:07:46 +0700

    OK DONE

--- Lab2/Prog_5_3.java
+++ Lab2/Prog_5_3.java
@@ -1,13 +1,14 @@
 public class Prog_5_3 {
     public static void main(String[]args) {
-        // แสดงข้อความ
-        System.out.printf("%-10s %10s\n","Kilograms","Pounds");
-        // for loop ให้kiloเท่ากับหนึ่ง ถ้าkiloยังน้อยกว่าหรือเท่ากับ 199 ให้ทำ code ใน {} หลังจากนั้น ไปทำ statement2 อีกที
-        for (int kilo=1 ;kilo<=199; kilo=kilo+2) {
-            //System.out.println(kilo);
-            double pound = kilo*2.2;
-            //System.out.println((float) pound);
-            System.out.printf("%-10d %,10.1f\n",kilo,pound);
+        System.out.printf("%s %10s","Kilograms", "Pounds\n");
+        for (int kilo=1; kilo<=199;kilo++) {
+            double pounds = kilo*2.2;
+            System.out.printf("%-10d %8.1f\n",kilo,pounds);
         }
     }
 }
+
+//
+//int number = 42;
+//printf("%10d\n", number);  // กำหนดให้พิมพ์ตัวเลขในช่องว่างกว้าง 10 ช่อง
+//    return 0;
