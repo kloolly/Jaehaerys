ceda4c909967dc22d20cc535a247d7ccb102ad29
kloolly <80373910+kloolly@users.noreply.github.com>
2024-06-18 15:20:40 +0700

    ยังไม่เสร็จ ยังงงอยู่

--- Lab2/Prog_5_3.java
+++ Lab2/Prog_5_3.java
@@ -1,11 +1,12 @@
 public class Prog_5_3 {
     public static void main(String[]args) {
+        // แสดงข้อความ
         System.out.printf("%-10s %10s\n","Kilograms","Pounds");
+        // for loop ให้kiloเท่ากับหนึ่ง ถ้าkiloยังน้อยกว่าหรือเท่ากับ 199 ให้ทำ code ใน {} หลังจากนั้น ไปทำ statement2 อีกที
         for (int kilo=1 ;kilo<=199; kilo=kilo+2) {
             //System.out.println(kilo);
             double pound = kilo*2.2;
             //System.out.println((float) pound);
-
             System.out.printf("%-10d %,10.1f\n",kilo,pound);
         }
     }
