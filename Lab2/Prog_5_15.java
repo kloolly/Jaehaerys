94682648a9e630fe90c4147d0eeb773306ae22c1
kloolly <80373910+kloolly@users.noreply.github.com>
2024-06-18 22:35:27 +0700

    OK DONE

---
+++ Lab2/Prog_5_15.java
@@ -0,0 +1,14 @@
+public class Prog_5_15 {
+    public static void main(String[] args) {
+        int counter = 0;
+        for (int i = 33; i <= 126; i++) {
+            System.out.printf("%c ", (char) i);
+            counter++;
+            if (counter % 10 == 0) {
+                System.out.println(" ");
+            }
+        }
+    }
+}
+
+// 33 - 126
