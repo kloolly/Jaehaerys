3ddc9fe7fdf623ba8a9adcee4ba40980a792610e
kloolly <80373910+kloolly@users.noreply.github.com>
2024-06-22 20:12:54 +0700

    wip

---
+++ Lab3/lecture/Dog.java
@@ -0,0 +1,13 @@
+// สร้าง class Dog
+public class Dog {
+//    สร้าง function ชื่อ bark
+//    โดยที่มี public คือ modifier และ void บอกว่าฟังก์ชันนี้ไม่มีการคืนค่ากลับไป
+//    bark() คือชื่อของฟังก์ชันและ () บอกว่าฟังก์ชันนี้ไม่ต้องการพารามิเตอร์ใดๆ ในการทำงาน
+    public void bark () {
+        System.out.println("This is bark ().");
+
+    }
+    public void wag () {
+        System.out.println("This is wag ().");
+    }
+}
--- selfLearning/src/w3/Main.java
+++ selfLearning/src/w3/Main.java
@@ -15,7 +15,5 @@ public class Main {
         System.out.println(myObj.x);
         System.out.println(myObj.y);
         System.out.println(myObj.z);
-
-
     }
 }
