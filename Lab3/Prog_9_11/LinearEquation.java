18bcd9724eaaa8fd600070b161e255a6d9c47ca9
kloolly <80373910+kloolly@users.noreply.github.com>
2024-06-24 10:19:59 +0700

    OK DONE add comment and fix parentheses in method getX, getY

--- Lab3/Prog_9_11/LinearEquation.java
+++ Lab3/Prog_9_11/LinearEquation.java
@@ -1,3 +1,4 @@
+//สร้าง class ประกาศตัวแปรเป็น private
 public class LinearEquation {
     private double a;
     private double b;
@@ -6,6 +7,7 @@ public class LinearEquation {
     private double e;
     private double f;
 
+//    สร้าง method LinearEquation ที่รับ 6 arguement มากำหนดค่าให้กับตัวแปร
     public LinearEquation(double a,double b, double c,double d, double e, double f) {
         this.a = a;
         this.b = b;
@@ -33,12 +35,15 @@ public class LinearEquation {
     public double getterF() {
         return f;
     }
+//    method isSolvable คืนค่าเป็น true ถ้า block code เป็นจริง
     public boolean isSolvable() {
         return (a*d)-(b*c)!=0;
     }
+//    method getX เมื่อถูกเรียกใช้งานจะทำงานใน block code แล้วคืนค่าคำตอบกลับไป
     public double getX() {
         return (e*d-b*f) / (a*d-b*c);
     }
+//    method getY เมื่อถูกเรียกใช้งานจะทำงานใน block code แล้วคืนค่าคำตอบกลับไป
     public double getY() {
         return (a*f-e*c) / (a*d-b*c);
     }
