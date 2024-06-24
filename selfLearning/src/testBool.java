8744b994235ad8f76275ed5d89bf560476070fae
kloolly <80373910+kloolly@users.noreply.github.com>
2024-06-24 09:48:04 +0700

    wip

---
+++ selfLearning/src/testBool.java
@@ -0,0 +1,10 @@
+public class testBool {
+    public static boolean isSolvable() {
+        return 2==2;
+    }
+
+    public static void main(String[]args) {
+        isSolvable();
+        System.out.println(isSolvable());
+    }
+}
