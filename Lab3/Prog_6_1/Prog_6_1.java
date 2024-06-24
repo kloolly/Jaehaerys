e703761ad94e0e0b204f8671c80ea701c93311a1
kloolly <80373910+kloolly@users.noreply.github.com>
2024-06-24 15:52:21 +0700

    OK DONE

--- Lab3/Prog_6_1/Prog_6_1.java
+++ Lab3/Prog_6_1/Prog_6_1.java
@@ -5,12 +5,12 @@ public class Prog_6_1 {
     }
 
     public static void main(String[]args) {
-//        int counter=0;
-        for (int i=1; i<=100; i++) {
-//            counter++;
-//            System.out.println(getPentagonalNumber(i));
-            if (i%10==0) {
-                System.out.print(getPentagonalNumber(i));
+        int counter=0;
+        for (int i=1 ; i<=100 ; i++) {
+//            System.out.print(getPentagonalNumber(i));
+            System.out.printf("% d",getPentagonalNumber(i));
+            counter++;
+            if (counter%10==0) {
                 System.out.println();
             }
         }
