03881ac6620391dc3078ed35ce94ede9bf1f267f
kloolly <bsjus104@gmail.com>
2024-07-18 23:13:57 +0700

    OK

--- Lab1/Prog_1_1.java
+++ Lab1/Prog_1_1.java
@@ -1,8 +1,17 @@
 public class Prog_1_1 {
     public static void main(String[]args) {
-        System.out.println("Welcome to java");
-        System.out.println("Welcome to Computer Science");
-        System.out.println("Programming is fun");
+//        for (int i =100; i>0; i*=2);
+//        i++;
+//        System.out.println(i);
+
+        String one = "1";
+        String two = "2";
+        String three = "3";
+        String four = "4";
+        String five = "5";
+
+        System.out.println(one+two+three+four+five);
+        System.out.println(1+2+3+4+5);
 
     }
 }
---
+++ selfLearning/src/testCalcu.java
@@ -0,0 +1,15 @@
+public class testCalcu {
+
+    public static void main(String[] args) {
+        double x = (1/2 +1/2);
+//    float y = 1.0/2.0;
+        double z = 4d + 5;
+        double u = 0.0 + 1/2;
+        double v = (0.0 + 1)/2;
+
+        double f = 212.0;
+        double c = (5/9) * (f-32);
+
+        System.out.println(c);
+    }
+}
